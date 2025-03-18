ca5aa9719ee8899f0ca8761dbc21f7fc3902eb3d
Lamyaa315 <lamyaalnahdi2@gmail.com>
2025-03-19 01:33:06 +0300

    mnm

--- Patient/cancel_appointment.php
+++ Patient/cancel_appointment.php
@@ -1,7 +1,17 @@
 <?php
 session_start();
-include '../db_connect.php'; // Database connection file
+$servername = "localhost"; 
+$username = "root"; 
+$password = "root"; 
+$database = "cura"; 
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 if (!isset($_SESSION['patient_id']) || !isset($_GET['id'])) {
     header("Location: patient_dashboard.php");
     exit();
